2c974dc5b55236ae9ef2b8cb0813dd87ddeba1f6
hammadsadi <hammad.sadi@yahoo.com>
2024-01-29 12:45:15 +0600

    Ticket Fare Calculator Task Done

--- conditional-task.js
+++ conditional-task.js
@@ -120,3 +120,32 @@ let num2 = 20;
 let result;
 result = num1 > num2 ? num1 * 2 : num1 + num2;
 console.log(result);
+
+/***
+
+Ticket fare Calculator
+    - Children (age < 10): free
+    - Students get a 50% discount
+    - Senior citizens (age >= 60) gets a 15% Discount
+    - Otherwise Regular ticket fare 800 tk
+*/
+
+let age = 65;
+let student = false;
+let ticketFee = 800;
+let seniorCitizensStatus = false;
+if (age < 10) {
+  console.log("Your Fare is Free");
+} else if ((age > 10) & (student === true)) {
+  let discount = (ticketFee * 50) / 100;
+  let payAmount = ticketFee - discount;
+  console.log("Students get a 50% discount, Your Ticket Fee Is :" + payAmount);
+} else if (age >= 60 && seniorCitizensStatus === true) {
+  let seniorDiscount = (ticketFee * 15) / 100;
+  let seniorDiscountAmount = ticketFee - seniorDiscount;
+  console.log(
+    "You are Senior citizens, Your Ticket Fee is:" + seniorDiscountAmount
+  );
+} else {
+  console.log("Regular ticket fare 800 tk");
+}
